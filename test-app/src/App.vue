5e414fe44bd34000879f2182958690e548eb9e45
Rasmus <rasmus.skovhaven@gmail.com>
2022-11-03 18:27:17 +0100

    working! missing create issue

--- test-app/src/App.vue
+++ test-app/src/App.vue
@@ -5,7 +5,12 @@ export default {
     return {
       issues: [],
       newIssueSummary: "",
-      newIssueDesc: ""
+      newIssueDesc: "",
+      accessToken: null,
+      jiraProjects: [],
+      project: {},
+      cloudids: [],
+      cloudid: null
     }
   },
   methods: {
@@ -39,13 +44,31 @@ export default {
         });
     },
 
+    // getIssues() {
+    //   fetch(`http://localhost:3000/rest/api/2/search?jql=project=${this.project.key}&maxResults=1000`,
+    //     {
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //         Accept: "application/json",
+    //         // Authorization: 'Basic ' + btoa('rasm5970@edu.ucl.dk' + ":" + import.meta.env.VITE_JIRA_TOKEN)
+    //         Authorization: `Bearer ${this.accessToken}`
+    //       },
+
+    //     })
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //       const { issues } = data
+    //       this.issues = issues
+    //     });
+    // },
+
     getIssues() {
-      fetch('http://localhost:3000/rest/api/2/search?jql=project=EARK&maxResults=1000',
+      fetch(`https://api.atlassian.com/ex/jira/${this.cloudid}"/rest/api/3/search?jql=project=${this.project.id}`,
         {
           headers: {
             "Content-Type": "application/json",
             Accept: "application/json",
-            Authorization: 'Basic ' + btoa('rasm5970@edu.ucl.dk' + ":" + import.meta.env.VITE_JIRA_TOKEN)
+            Authorization: `Bearer ${this.accessToken}`
           },
 
         })
@@ -53,13 +76,57 @@ export default {
         .then((data) => {
           const { issues } = data
           this.issues = issues
+          console.log("issues", this.issues)
         });
     },
+
+    getProject(cloudid) {
+      fetch(`https://api.atlassian.com/ex/jira/${cloudid}/rest/api/2/project`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${this.accessToken}`
+          },
+
+        })
+        .then((response) => response.json())
+        .then((data) => {
+          this.jiraProjects = data
+          console.log("jira projects", this.jiraProjects)
+
+          /* Get first project */
+          fetch(this.jiraProjects[0].self,
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                Authorization: `Bearer ${this.accessToken}`
+              },
+
+            })
+            .then((response) => response.json())
+            .then((data) => {
+              this.project = data
+              console.log("this project", this.project)
+              this.getIssues()
+            });
+        });
+    },
+
+    signIn(params) {
+      this.accessToken = params.token
+      this.cloudids = [...params.data]
+      this.cloudid = this.cloudids[0].id
+      console.log("cloudids", this.cloudids)
+
+      // this.getUser();
+      this.getProject(this.cloudid)
+    }
   },
   mounted() {
-    this.getIssues()
     const params = new URLSearchParams(window.location.search);
-    if (params.get("q")) console.log("lol", JSON.parse(decodeURIComponent(params.get("q"))))
+    if (params.get("q")) this.signIn(JSON.parse(decodeURIComponent(params.get("q"))))
 
 
   }
@@ -73,10 +140,14 @@ export default {
       href="https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id=f0rb1sOMiQ9pPK860ygqqZ87hKHfHeyx&scope=read%3Ajira-work%20manage%3Ajira-project%20manage%3Ajira-configuration%20read%3Ajira-user%20write%3Ajira-work&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcallback&state=teststate&response_type=code&prompt=consent">login</a>
     <h1>Issues</h1>
     <ul>
-      <li v-for="issue in issues" :key="issue.key">
+      <li v-for="issue in issues" :key="issue.id">
         <strong>{{ issue.key }} : {{ issue.fields.summary }}</strong>
         <br />
-        <small>{{ issue.fields.description }}</small>
+        <div v-if="issue.fields.description && issue.fields.description.content.length">
+          <small v-for="desc in issue.fields.description.content">
+            <p v-for="text in desc.content">{{ text.text }}</p>
+          </small>
+        </div>
       </li>
     </ul>
 
