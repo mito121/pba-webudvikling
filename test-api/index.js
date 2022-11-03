472b34d8acc13c7daffb8708fa6459e476803f11
Rasmus <rasmus.skovhaven@gmail.com>
2022-11-03 18:34:45 +0100

    Cleanup

--- test-api/index.js
+++ test-api/index.js
@@ -1,27 +1,15 @@
+if (process.env.NODE_ENV !== "production") {
+    /* Get .env varibales */
+    require("dotenv").config();
+}
+
+
 const PORT = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
 const axios = require("axios");
 const cors = require("cors");
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const passport = require('passport')
-const initializePassport = require('./config/passport-setup')
-const session = require('express-session')
-const flash = require('express-flash')
-
-/* Passport */
-initializePassport(
-    passport
-);
-
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true }
-})); // session secret
-app.use(passport.initialize());
-app.use(flash()); // use connect-flash for flash messages stored in session
 
 const API_SERVICE_URL = "https://eark.atlassian.net"
 
@@ -66,28 +54,51 @@ app.post("/issue", async (req, res) => {
     })
 });
 
-
-/* Oauth */
-app.get('/auth/atlassian-oauth',
-    passport.authenticate('atlassian-oauth'),
-    (req, res) => {
-        // The request will be redirected to the Atlassian app for authentication, so this
-        // function will not be called.
-        console.log("frol?")
-    });
-
-app.get('/auth/atlassian-oauth/callback',
-    passport.authenticate('atlassian-oauth', { failureRedirect: '/login' }),
-    function (req, res) {
-        console.log("frol???")
-        res.redirect('/');
-    });
-
-
-app.get('/login', passport.authenticate('atlassian-oauth', {
-    failWithError: true,
-    // scope: ['profile']
-}))
+/* OAuth */
+/* https://developer.atlassian.com/cloud/jira/platform/oauth-2-3lo-apps/ */
+app.get('/auth/callback', (req, res) => {
+    axios({
+        method: 'post',
+        url: 'https://auth.atlassian.com/oauth/token',
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        data: {
+            'grant_type': 'authorization_code',
+            'client_id': process.env.JIRA_CLIENT_ID,
+            'client_secret': process.env.JIRA_CLIENT_SECRET,
+            'code': req.query.code,
+            'redirect_uri': 'http://localhost:3000/auth/callback'
+        },
+    }).then((response) => {
+        console.log("res", response.data)
+        const access_token = response.data.access_token
+        /* Get cloudids */
+        axios({
+            method: "get",
+            url: "https://api.atlassian.com/oauth/token/accessible-resources",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${access_token}}]`
+            }
+        }).then((cloudidRes) => {
+            const resultObj = {
+                data: cloudidRes.data,
+                token: access_token
+            }
+            const encodedData = encodeURIComponent(JSON.stringify(resultObj))
+            res.redirect(`http://127.0.0.1:5173?q=${encodedData}`)
+        }).catch(e => {
+            console.log("error", e)
+            res.sendStatus(500)
+        })
+    }).catch(e => {
+        console.log("error", e)
+        res.sendStatus(500)
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`listening on *:${PORT}`);
